9f783fb7517f170f0aba810e8fd9c3b0b3a96944
Jiwan <jguegant@gmail.com>
2013-11-11 22:35:15 +0100

    Created a bootstrap building file (build.js)

--- build/build.js
+++ build/build.js
@@ -1,1 +1,29 @@
-﻿var exec = require('child_process').exec;
+﻿/// <reference path="./definitions/node.d.ts"/>
+var exec = require('child_process').exec;
+
+var buildSteps = [
+    {
+        message: 'Compiling the server...',
+        command: 'node ./node_modules/typescript/bin/tsc server/application.ts \
+                  --out bin/server.js \
+                  --target ES5 --sourcemap'
+    },
+    {
+        message: 'Compiling the client...',
+        command: 'node ./node_modules/typescript/bin/tsc client/mycalendar.ts \
+                  --out bin/mycalendar.js \
+                  --target ES5 --sourcemap'
+    }
+];
+
+buildSteps.forEach(function (value, index) {
+    exec(function (err, stdout, stderr) {
+        if (err) {
+            console.log(err);
+            console.log(stdout);
+            console.log(stderr);
+        }
+    });
+});
+
+console.log('Build finished!');
--- build/build.ts
+++ build/build.ts
@@ -1,1 +1,31 @@
-﻿var exec = require('child_process').exec;
+﻿/// <reference path="./definitions/node.d.ts"/>
+
+var exec = require('child_process').exec;
+
+var buildSteps: { message: string; command: string }[] = [
+    {
+        message: 'Compiling the server...', 
+        command: 'node ./node_modules/typescript/bin/tsc server/application.ts \
+                  --out bin/server.js \
+                  --target ES5 --sourcemap'
+    },
+
+    {
+        message: 'Compiling the client...', 
+        command: 'node ./node_modules/typescript/bin/tsc client/mycalendar.ts \
+                  --out bin/mycalendar.js \
+                  --target ES5 --sourcemap'
+    }
+];
+
+buildSteps.forEach((value: { message: string; command: string }, index: number): void => {
+    exec((err: any, stdout: any, stderr: any) => {
+        if (err) {
+            console.log(err);
+            console.log(stdout);
+            console.log(stderr);
+        }
+    });
+});
+
+console.log('Build finished!');
