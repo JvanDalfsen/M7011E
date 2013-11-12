9060f5b7c8486434dca50a4a871614dc64fe0cd9
Jiwan <jguegant@gmail.com>
2013-11-12 01:24:01 +0100

    Delete mycalendar.js

--- client/mycalendar.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="./definitions/jquery.d.ts"/>
-// Start the script when the page is ready.
-$(function () {
-    $('#test-button').click(function (event) {
-        alert('It\'s Fika time!');
-    });
-});
