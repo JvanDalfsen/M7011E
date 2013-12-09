f1d9a90d902dbe7d09dfbdb6c4cd2b50583dc09e
Jiwan <jguegant@gmail.com>
2013-12-09 14:45:07 +0100

    Added js files for client

---
+++ client/mycalendar.js
@@ -0,0 +1,83 @@
+﻿/// <reference path="./definitions/jquery.d.ts"/>
+/// <reference path="./repository.ts"/>
+/// <reference path="./definitions/jqueryui.d.ts"/>
+/// <reference path="./definitions/fullCalendar.d.ts"/>
+/// <reference path="./ui/panels/document-manager.ts"/>
+/// <reference path="./ui/panels/calendar-manager.ts"/>
+/// <reference path="./ui/panel-host.ts"/>
+// Start the script when the page is ready.
+$(function () {
+    var userButton = $('#user-settings-button');
+    var mainMenu = $('#main-menu');
+
+    // open and close the account-menu
+    userButton.click(function (event) {
+        var state = userButton.attr('selected');
+
+        if (typeof state === 'undefined' || (state) === false) {
+            userButton.attr('selected', true);
+            mainMenu.show().animate({ width: 240 }, 200, function () {
+                mainMenu.children('.hidable-menu').show();
+            });
+            $('body').width($('body').width() - 100);
+        } else {
+            mainMenu.children('.hidable-menu').hide();
+            mainMenu.show().animate({ width: 80 }, 200, function () {
+                userButton.removeAttr('selected');
+            });
+            $('body').width($('body').width() + 100);
+        }
+
+        console.log("type: ");
+    });
+
+    // Database tests!
+    MyCalendar.calendarsRepository.create({ name: 'test', events: [] }).done(function (myCalendar) {
+        console.log(myCalendar.getRefId());
+        myCalendar.name = 'yo';
+        MyCalendar.calendarsRepository.save(myCalendar).done(function () {
+            MyCalendar.calendarsRepository.findById(myCalendar.getRefId()).done(function (myCalendar2) {
+                console.log(myCalendar2.name);
+            });
+        });
+
+        MyCalendar.calendarsRepository.find({}).done(function (calendars) {
+            calendars.map(function (value, index, array) {
+                console.log(value.getRefId());
+                MyCalendar.calendarsRepository.delete(value);
+            });
+        });
+
+        MyCalendar.calendarsRepository.find({}).done(function (calendars) {
+            calendars.map(function (value, index, array) {
+                console.log(value.getRefId());
+            });
+        });
+
+        var event = new MyCalendar.Models.Event();
+        event.name = 'test';
+        event.description = 'test';
+        event.location = 'test';
+        event.begin = new Date(2014, 1, 1);
+        event.end = new Date(2014, 1, 2);
+        event.documents = [];
+
+        MyCalendar.eventsRepository.save(event).done(function (event) {
+            var calendar = new MyCalendar.Models.Calendar();
+
+            calendar.name = 'test';
+            calendar.events = [new MyCalendar.Models.Ref(event.getRefId(), MyCalendar.eventsRepository)];
+
+            MyCalendar.calendarsRepository.save(calendar).done(function (calendar) {
+                console.log('saved!');
+            });
+        });
+    });
+
+    MyCalendar.UI.PanelHost.getInstance().pushPanel(new MyCalendar.UI.Panels.DocumentManagerPanel(), function () {
+        MyCalendar.UI.PanelHost.getInstance().pushPanel(new MyCalendar.UI.Panels.DocumentManagerPanel(), function () {
+            MyCalendar.UI.PanelHost.getInstance().pushPanel(new MyCalendar.UI.Panels.DocumentManagerPanel());
+        });
+    });
+    // MyCalendar.UI.PanelHost.getInstance().pushPanel(new MyCalendar.UI.Panels.CalendarManagerPanel());
+});
---
+++ client/repository.js
@@ -0,0 +1,174 @@
+/// <reference path="./definitions/jquery.d.ts"/>
+/// <reference path="./models/model.ts"/>
+/// <reference path="./models/calendar.ts"/>
+/// <reference path="./models/event.ts"/>
+var MyCalendar;
+(function (MyCalendar) {
+    var Repository = (function () {
+        function Repository(_cstr) {
+            this._cstr = _cstr;
+            // _cstr: A trick to call the right constructor.
+            // http://stackoverflow.com/questions/17382143/how-to-create-a-new-object-from-type-parameter-in-generic-class-in-typescript
+            // This way we can compare the type of a dummy with instanceof.
+            this._dummie = new this._cstr();
+        }
+        Repository.prototype.create = function (value) {
+            var _this = this;
+            return this.callAPI('POST', '', value).then(function (json) {
+                return _this.buildModel(json);
+            });
+        };
+
+        Repository.prototype.find = function (query) {
+            var _this = this;
+            return this.callAPI('GET', '', query).then(function (json) {
+                return json.map(function (result, index, array) {
+                    return _this.buildModel(result);
+                });
+            });
+        };
+
+        Repository.prototype.findById = function (id) {
+            var _this = this;
+            return this.callAPI('GET', '/' + id).then(function (json) {
+                return _this.buildModel(json);
+            });
+        };
+
+        Repository.prototype.update = function (id, value) {
+            var _this = this;
+            return this.callAPI('PUT', '/' + id, value).then(function (json) {
+                return _this.buildModel(json);
+            });
+        };
+
+        Repository.prototype.save = function (obj) {
+            var _this = this;
+            if (this.isCalendar()) {
+                // Transform the ref into strings.
+                (obj).events = (obj).events.map(this.extractRef);
+            }
+
+            if (this.isEvent()) {
+                // Transform the ref into strings.
+                (obj).documents = (obj).documents.map(this.extractRef);
+            }
+
+            return this.callAPI('PUT', '/' + obj.getRefId(), obj).then(function (json) {
+                var value = _this.buildModel(json);
+
+                for (var prop in value) {
+                    // Update the _id of the object.
+                    obj[prop] = value[prop];
+                }
+
+                return obj;
+            });
+        };
+
+        Repository.prototype.delete = function (obj) {
+            return this.deleteById(obj.getRefId());
+        };
+
+        Repository.prototype.deleteById = function (id) {
+            var _this = this;
+            return this.callAPI('DELETE', '/' + id).then(function (json) {
+                return _this.buildModel(json);
+            });
+        };
+
+        Repository.prototype.buildModel = function (json) {
+            if (this.isCalendar()) {
+                var calendar = new MyCalendar.Models.Calendar();
+                calendar._id = json._id;
+                calendar.name = json.name;
+
+                // Copy the references.
+                var events = json.events;
+
+                calendar.events = events.map(function (value, index, array) {
+                    return new MyCalendar.Models.Ref(value, MyCalendar.eventsRepository);
+                });
+
+                return calendar;
+            } else if (this.isDocument()) {
+                var document = new MyCalendar.Models.Document();
+
+                for (var prop in json) {
+                    document[prop] = json[prop];
+                }
+
+                return document;
+            } else if (this.isEvent()) {
+                var event = new MyCalendar.Models.Event();
+                event._id = json._id;
+                event.begin = json.being;
+                event.end = json.end;
+                event.name = json.name;
+                event.description = json.description;
+
+                // Copy the references.
+                var documents = json.documents;
+
+                event.documents = documents.map(function (value, index, array) {
+                    return new MyCalendar.Models.Ref(value, MyCalendar.documentsRepository);
+                });
+
+                return event;
+            }
+
+            return null;
+        };
+
+        Repository.prototype.getRoute = function () {
+            if (this.isCalendar()) {
+                return '/calendars';
+            } else if (this.isEvent()) {
+                return '/events';
+            } else if (this.isDocument()) {
+                return '/documents';
+            }
+
+            throw 'no route avaiblable for this type';
+
+            return null;
+        };
+
+        Repository.prototype.callAPI = function (type, url, data) {
+            if (typeof url === "undefined") { url = ''; }
+            if (typeof data === "undefined") { data = {}; }
+            return $.ajax({
+                type: type,
+                url: Repository.ApiRoot + this.getRoute() + url,
+                data: data,
+                dataType: 'json',
+                success: function (json, textStatus, jqXHR) {
+                    return json;
+                }
+            });
+        };
+
+        Repository.prototype.isCalendar = function () {
+            return this._dummie instanceof MyCalendar.Models.Calendar;
+        };
+
+        Repository.prototype.isEvent = function () {
+            return this._dummie instanceof MyCalendar.Models.Event;
+        };
+
+        Repository.prototype.isDocument = function () {
+            return this._dummie instanceof MyCalendar.Models.Document;
+        };
+
+        Repository.prototype.extractRef = function (value, index, array) {
+            return value.id;
+        };
+        Repository.ApiRoot = '/api';
+        return Repository;
+    })();
+    MyCalendar.Repository = Repository;
+
+    MyCalendar.eventsRepository = new Repository(MyCalendar.Models.Event);
+    MyCalendar.calendarsRepository = new Repository(MyCalendar.Models.Calendar);
+    MyCalendar.documentsRepository = new Repository(MyCalendar.Models.Document);
+})(MyCalendar || (MyCalendar = {}));
